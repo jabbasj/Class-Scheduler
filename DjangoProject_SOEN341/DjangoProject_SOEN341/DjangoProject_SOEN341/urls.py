bead27625f1ff433cd210ebd6b73d6c1c49f5947
Admin <Admin>
2016-03-05 18:24:48 -0500

    working web layout, with local sql database integration and models

---
+++ DjangoProject_SOEN341/DjangoProject_SOEN341/DjangoProject_SOEN341/urls.py
@@ -0,0 +1,61 @@
+﻿"""
+Definition of urls for DjangoProject_SOEN341.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+
+
+from app.views import home
+from app.views import contact
+from app.views import about
+from app.views import profile
+from app.views import record
+from app.views import schedule
+from app.views import workshop
+
+from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
+
+# Uncomment the next lines to enable the admin:
+from django.conf.urls import include
+from django.contrib import admin
+from django.contrib.admindocs import urls as admindocs_urls
+
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', home, name='home'),
+    url(r'^profile', profile, name='profile'),
+    url(r'^record', record, name='record'),
+    url(r'^workshop', workshop, name='workshop'),
+    url(r'^schedule', schedule, name='schedule'),
+    url(r'^contact$', contact, name='contact'),
+    url(r'^about', about, name='about'),
+    url(r'^login/$',
+        login,
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        logout,
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include(admindocs_urls)),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+]
---
+++ DjangoProject_SOEN341/DjangoProject_SOEN341/app/views.py
@@ -0,0 +1,108 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+from app.models import Students
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def profile(request):
+    """Renders the profile page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/profile.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Profile',
+            'student_name': Students.objects.get(email=request.user),
+            'message':'Your student profile page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def record(request):
+    """Renders the academic record page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/records.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Academic Record',
+            'message':'Your student record page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def schedule(request):
+    """Renders the schedule page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/schedule.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Schedule',
+            'message':'Your schedule page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def workshop(request):
+    """Renders the workshop page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/workshop.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Workshop',
+            'message':'Your workshop page.',
+            'year':datetime.now().year,
+        })
+    )
