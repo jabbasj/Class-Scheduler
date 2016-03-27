786688dc325125c34925dd17d9e0f54a1bfde649
jabbasj <abbasj92@gmail.com>
2016-03-27 04:00:17 -0400

    added buttons in profile for changing email and password, pops 'modal' above current layout, greys out bottom layout. Changes reflected immediately. Error/success message pops up

--- DjangoProject_SOEN341/DjangoProject_SOEN341/app/models.py
+++ DjangoProject_SOEN341/DjangoProject_SOEN341/app/models.py
@@ -90,3 +90,19 @@ class Sequence(models.Model):
         managed = False
         db_table = 'sequence'
 
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
